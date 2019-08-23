556e0ddc27354a221751a8ecc717e79c9015e498
dcljr <dcljr@me.com>
2019-08-23 13:38:17 -0700

    added background tag

---
+++ BeatSaberMarkupLanguage/Tags/BackgroundTag.cs
@@ -0,0 +1,28 @@
+﻿using BeatSaberMarkupLanguage.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatSaberMarkupLanguage.Tags
+{
+    public class BackgroundTag : BSMLTag
+    {
+        public override string[] Aliases => new[] { "bg", "background", "div" };
+
+        public override GameObject CreateObject(Transform parent)
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.name = "BSMLBackground";
+            gameObject.transform.SetParent(parent, false);
+            gameObject.AddComponent<ContentSizeFitter>();
+            gameObject.AddComponent<Backgroundable>();
+            //RectTransform rectTransform = gameObject.transform as RectTransform;
+            //rectTransform.sizeDelta = new Vector2(BSMLParser.SCREEN_WIDTH, BSMLParser.SCREEN_HEIGHT);
+            return gameObject;
+        }
+    }
+}
