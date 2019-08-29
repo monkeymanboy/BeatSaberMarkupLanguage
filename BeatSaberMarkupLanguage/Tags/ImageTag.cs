e43fad2b7e6e420178e2a0c90dd084021f7e8a75
Auros <41306347+aurosnex@users.noreply.github.com>
2019-08-29 18:17:58 -0400

    Image Type Support

---
+++ BeatSaberMarkupLanguage/Tags/ImageTag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BeatSaberMarkupLanguage.Tags;
+using UnityEngine.UI;
+using BeatSaberMarkupLanguage;
+
+namespace BeatSaberMarkupLanguage.Tags
+{
+    public class ImageTag : BSMLTag
+    {
+        public override string[] Aliases => new[] { "image" };
+
+        public override GameObject CreateObject(Transform parent)
+        {
+            GameObject gameObject = new GameObject("ATImage");
+
+            RawImage image = gameObject.AddComponent<RawImage>();
+            image.material = Utilities.ImageResources.NoGlowMat;
+            image.rectTransform.sizeDelta = new Vector2(20f, 20f);
+            image.rectTransform.SetParent(parent, false);
+            image.texture = Utilities.ImageResources.BlankSprite.texture;
+
+            return gameObject;
+        }
+    }
+}
