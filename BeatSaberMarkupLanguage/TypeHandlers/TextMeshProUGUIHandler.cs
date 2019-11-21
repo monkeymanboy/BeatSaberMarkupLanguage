ecad05d1e1351dcce55934c334c9ef1325104396
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-11-21 14:45:42 -0600

    Generic TypeHandler

--- BeatSaberMarkupLanguage/TypeHandlers/TextMeshProUGUIHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/TextMeshProUGUIHandler.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 namespace BeatSaberMarkupLanguage.TypeHandlers
 {
     [ComponentHandler(typeof(TextMeshProUGUI))]
-    public class TextMeshProUGUIHandler : TypeHandler
+    public class TextMeshProUGUIHandler : TypeHandler<TextMeshProUGUI>
     {
         public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
         {
@@ -23,41 +23,17 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
             { "strikethrough", new[]{ "strikethrough" } }
         };
 
-        public Dictionary<string, Action<TextMeshProUGUI, string>> Setters => new Dictionary<string, Action<TextMeshProUGUI, string>>()
+        public override Dictionary<string, Action<TextMeshProUGUI, string>> Setters => new Dictionary<string, Action<TextMeshProUGUI, string>>()
         {
-            {"text", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.text = value) },
-            {"fontSize", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.fontSize = Parse.Float(value)) },
-            {"alignment", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.alignment = (TextAlignmentOptions)Enum.Parse(typeof(TextAlignmentOptions), value)) },
-            {"overflowMode", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.overflowMode = (TextOverflowModes)Enum.Parse(typeof(TextOverflowModes), value)) },
-            {"bold", new Action<TextMeshProUGUI, string>((textMesh, value) => textMesh.fontStyle = SetStyle(textMesh.fontStyle, FontStyles.Bold, value)) },
-            {"italics", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.fontStyle = SetStyle(textMesh.fontStyle, FontStyles.Italic, value))  },
-            {"underlined", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.fontStyle = SetStyle(textMesh.fontStyle, FontStyles.Underline, value))  },
-            {"strikethrough", new Action<TextMeshProUGUI,string>((textMesh, value) => textMesh.fontStyle = SetStyle(textMesh.fontStyle, FontStyles.Strikethrough, value))  },
+            {"text", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).text = value) },
+            {"fontSize", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).fontSize = Parse.Float(value)) },
+            {"alignment", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).alignment = (TextAlignmentOptions)Enum.Parse(typeof(TextAlignmentOptions), value)) },
+            {"overflowMode", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).overflowMode = (TextOverflowModes)Enum.Parse(typeof(TextOverflowModes), value)) },
+            {"bold", new Action<TextMeshProUGUI, string>((textMesh, value) => (textMesh).fontStyle = SetStyle((textMesh).fontStyle, FontStyles.Bold, value)) },
+            {"italics", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).fontStyle = SetStyle((textMesh).fontStyle, FontStyles.Italic, value))  },
+            {"underlined", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).fontStyle = SetStyle((textMesh).fontStyle, FontStyles.Underline, value))  },
+            {"strikethrough", new Action<TextMeshProUGUI,string>((textMesh, value) => (textMesh).fontStyle = SetStyle((textMesh).fontStyle, FontStyles.Strikethrough, value))  },
         };
-
-        public override void HandleType(Component obj, Dictionary<string, string> data, BSMLParserParams parserParams)
-        {
-            TextMeshProUGUI textMesh = obj as TextMeshProUGUI;
-            NotifyUpdater updater = null;
-            if (parserParams.host is INotifiableHost notifyHost)
-            {
-                Logger.log?.Warn($"host is INotifiableHost");
-                updater = textMesh.gameObject.AddComponent<NotifyUpdater>();
-                updater.NotifyHost = notifyHost;
-                updater.ActionDict = new Dictionary<string, Action<object>>();
-            }
-            foreach (var pair in data)
-            {
-                if (Setters.TryGetValue(pair.Key, out var action))
-                {
-                    action.Invoke(textMesh, pair.Value);
-                    if (updater != null && parserParams.propertyMap.TryGetValue(pair.Key, out var prop))
-                        updater?.ActionDict.Add(prop.Name, val => action.Invoke(textMesh, val.ToString()));
-                }
-                else
-                    Logger.log?.Warn($"Tag {pair.Key} not supported for {nameof(TextMeshProUGUI)}");
-            }
-        }
 
         private static FontStyles SetStyle(FontStyles existing, FontStyles modifyStyle, string flag)
         {
--- BeatSaberMarkupLanguage/TypeHandlers/TypeHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/TypeHandler.cs
@@ -1,6 +1,9 @@
-﻿using BeatSaberMarkupLanguage.Parser;
+﻿using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Notify;
+using BeatSaberMarkupLanguage.Parser;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace BeatSaberMarkupLanguage.TypeHandlers
@@ -11,6 +14,40 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
 
         public virtual void HandleType(Component obj, Dictionary<string, string> data, BSMLParserParams parserParams) { }
         public virtual void HandleTypeAfterChildren(Component obj, Dictionary<string, string> data, BSMLParserParams parserParams) { }
+    }
+
+    public abstract class TypeHandler<T> : TypeHandler
+        where T : Component
+    {
+        public abstract Dictionary<string, Action<T, string>> Setters { get; }
+
+        public override void HandleType(Component obj, Dictionary<string, string> data, BSMLParserParams parserParams)
+        {
+            if (obj is T castObj)
+                HandleType(castObj, data, parserParams);
+        }
+
+        public virtual void HandleType(T obj, Dictionary<string, string> data, BSMLParserParams parserParams)
+        {
+            NotifyUpdater updater = null;
+            if (parserParams.host is INotifiableHost notifyHost)
+            {
+                updater = obj.gameObject.AddComponent<NotifyUpdater>();
+                updater.NotifyHost = notifyHost;
+                updater.ActionDict = new Dictionary<string, Action<object>>();
+            }
+            foreach (KeyValuePair<string, string> pair in data)
+            {
+                if (Setters.TryGetValue(pair.Key, out Action<T, string> action))
+                {
+                    action.Invoke(obj, pair.Value);
+                    if (updater != null && parserParams.propertyMap.TryGetValue(pair.Key, out PropertyInfo prop))
+                        updater?.ActionDict.Add(prop.Name, val => action.Invoke(obj, val.ToString()));
+                }
+                else
+                    Logger.log?.Warn($"Tag {pair.Key} not supported for {obj.GetType().Name}");
+            }
+        }
     }
 
     public class ComponentHandler : Attribute
