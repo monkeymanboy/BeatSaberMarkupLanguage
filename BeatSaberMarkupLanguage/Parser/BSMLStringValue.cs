21cbeddc5e94c1c4c1cb7816f5b45a4ae16bca40
dcljr <dcljr@me.com>
2019-12-10 00:02:24 -0800

    added define macro

---
+++ BeatSaberMarkupLanguage/Parser/BSMLStringValue.cs
@@ -0,0 +1,23 @@
+﻿namespace BeatSaberMarkupLanguage.Parser
+{
+    public class BSMLStringValue : BSMLValue
+    {
+        private string value;
+
+        public BSMLStringValue(string value)
+        {
+            this.value = value;
+        }
+
+        public override object GetValue()
+        {
+            return value;
+        }
+
+        public override void SetValue(object value)
+        {
+            Logger.log.Warn("You should not be using a macro defined value in a way that SetValue is called");
+            this.value = value.ToString();
+        }
+    }
+}
