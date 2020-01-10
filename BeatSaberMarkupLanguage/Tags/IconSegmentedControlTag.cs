2c6ecd6b463d04f97f028a78c37d01971ff53fcf
Zingabopp <Zingabopp@gmail.com>
2020-01-10 14:51:13 -0600

    Fixed how DiContainer is retreived

--- BeatSaberMarkupLanguage/Tags/IconSegmentedControlTag.cs
+++ BeatSaberMarkupLanguage/Tags/IconSegmentedControlTag.cs
@@ -15,7 +15,7 @@ namespace BeatSaberMarkupLanguage.Tags
             IconSegmentedControl prefab = Resources.FindObjectsOfTypeAll<IconSegmentedControl>().First(x => x.name == "BeatmapCharacteristicSegmentedControl");
             IconSegmentedControl iconSegmentedControl = MonoBehaviour.Instantiate(prefab, parent, false);
             iconSegmentedControl.name = "BSMLIconSegmentedControl";
-            iconSegmentedControl.SetPrivateField("_container", prefab.GetPrivateField<DiContainer>("_container"));
+            iconSegmentedControl.SetPrivateField("_container", Resources.FindObjectsOfTypeAll<IconSegmentedControl>().First(x => x.GetPrivateField<DiContainer>("_container") != null).GetPrivateField<DiContainer>("_container"));
             (iconSegmentedControl.transform as RectTransform).anchoredPosition = new Vector2(0, 0);
             foreach (Transform transform in iconSegmentedControl.transform)
             {
--- BeatSaberMarkupLanguage/Tags/TextSegmentedControlTag.cs
+++ BeatSaberMarkupLanguage/Tags/TextSegmentedControlTag.cs
@@ -15,7 +15,7 @@ namespace BeatSaberMarkupLanguage.Tags
             TextSegmentedControl prefab = Resources.FindObjectsOfTypeAll<TextSegmentedControl>().First(x => x.name == "BeatmapDifficultySegmentedControl");
             TextSegmentedControl textSegmentedControl = MonoBehaviour.Instantiate(prefab, parent, false);
             textSegmentedControl.name = "BSMLTextSegmentedControl";
-            textSegmentedControl.SetPrivateField("_container", prefab.GetPrivateField<DiContainer>("_container"));
+            textSegmentedControl.SetPrivateField("_container", Resources.FindObjectsOfTypeAll<TextSegmentedControl>().First(x => x.GetPrivateField<DiContainer>("_container") != null).GetPrivateField<DiContainer>("_container"));
             (textSegmentedControl.transform as RectTransform).anchoredPosition = new Vector2(0, 0);
             foreach (Transform transform in textSegmentedControl.transform)
             {
