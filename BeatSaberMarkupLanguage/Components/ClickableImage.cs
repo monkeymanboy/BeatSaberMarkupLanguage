f5559e0015679e781f2ca7f57ed2aa253eb3c7df
brian91292 <brian@yur.fit>
2020-04-04 11:06:32 -0500

    Add ClickableImage

---
+++ BeatSaberMarkupLanguage/Components/ClickableImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace BeatSaberMarkupLanguage.Components
+{
+    public class ClickableImage : Image, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IEventSystemHandler
+    {
+        public Color highlightColor = new Color(0.60f, 0.80f, 1);
+        public Color defaultColor = Color.white;
+        public Action<PointerEventData> OnClickEvent, PointerEnterEvent, PointerExitEvent;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            color = defaultColor;
+            OnClickEvent?.Invoke(eventData);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            color = highlightColor;
+            PointerEnterEvent?.Invoke(eventData);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            color = defaultColor;
+            PointerExitEvent?.Invoke(eventData);
+        }
+    }
+}
---
+++ BeatSaberMarkupLanguage/TypeHandlers/ClickableImageHandler.cs
@@ -0,0 +1,41 @@
+﻿using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Parser;
+using System;
+using System.Collections.Generic;
+using static BeatSaberMarkupLanguage.BSMLParser;
+
+namespace BeatSaberMarkupLanguage.TypeHandlers
+{
+    [ComponentHandler(typeof(ClickableImage))]
+    public class ClickableImageHandler : TypeHandler
+    {
+        public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
+        {
+            { "onClick", new[]{"on-click"} },
+            { "clickEvent", new[]{"click-event", "event-click"} }
+        };
+
+        public override void HandleType(ComponentTypeWithData componentType, BSMLParserParams parserParams)
+        {
+            ClickableImage clickableImage = componentType.component as ClickableImage;
+            if (componentType.data.TryGetValue("onClick", out string onClick))
+            {
+                clickableImage.OnClickEvent += delegate
+                {
+                    if (!parserParams.actions.TryGetValue(onClick, out BSMLAction onClickAction))
+                        throw new Exception("on-click action '" + onClick + "' not found");
+
+                    onClickAction.Invoke();
+                };
+            }
+
+            if (componentType.data.TryGetValue("clickEvent", out string clickEvent))
+            {
+                clickableImage.OnClickEvent += delegate
+                {
+                    parserParams.EmitEvent(clickEvent);
+                };
+            }
+        }
+    }
+}
