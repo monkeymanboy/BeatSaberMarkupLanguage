53f141bb40cd36132bdc6690c8f906cd67e6a71d
brian91292 <brian@yur.fit>
2020-04-04 10:47:35 -0500

    Add color property to image handler

--- BeatSaberMarkupLanguage/TypeHandlers/ImageHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/ImageHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace BeatSaberMarkupLanguage.TypeHandlers
@@ -10,14 +11,24 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
         public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
         {
             { "image", new[] { "source" , "src" } },
-            { "preserveAspect", new[] { "preserve-aspect" } }
+            { "preserveAspect", new[] { "preserve-aspect" } },
+            { "color", new[] { "color" } }
         };
 
         public override Dictionary<string, Action<Image, string>> Setters => new Dictionary<string, Action<Image, string>>()
         {
             { "image", new Action<Image, string>(BeatSaberUI.SetImage) },
-            { "preserveAspect", new Action<Image, string>((image, preserveAspect) => image.preserveAspect = bool.Parse(preserveAspect)) }
+            { "preserveAspect", new Action<Image, string>((image, preserveAspect) => image.preserveAspect = bool.Parse(preserveAspect)) },
+            { "color", new Action<Image, string>((image, color) => image.color = GetColor(color)) }
 
         };
+
+        private static Color GetColor(string colorStr)
+        {
+            if (ColorUtility.TryParseHtmlString(colorStr, out Color color))
+                return color;
+            Logger.log?.Warn($"Color {colorStr}, is not a valid color.");
+            return Color.white;
+        }
     }
 }
