2bde07f21ebe369576bc06b6d3c5f36e117d3e8f
brian91292 <brian@yur.fit>
2020-04-04 11:26:30 -0500

    Allow default/highlight color to be set via bsml for clickable image/text

--- BeatSaberMarkupLanguage/TypeHandlers/ClickableImageHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/ClickableImageHandler.cs
@@ -2,17 +2,26 @@
 using BeatSaberMarkupLanguage.Parser;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using static BeatSaberMarkupLanguage.BSMLParser;
 
 namespace BeatSaberMarkupLanguage.TypeHandlers
 {
     [ComponentHandler(typeof(ClickableImage))]
-    public class ClickableImageHandler : TypeHandler
+    public class ClickableImageHandler : TypeHandler<ClickableImage>
     {
         public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
         {
             { "onClick", new[]{"on-click"} },
-            { "clickEvent", new[]{"click-event", "event-click"} }
+            { "clickEvent", new[]{"click-event", "event-click"} },
+            { "highlightColor", new[]{ "highlight-color" } },
+            { "defaultColor", new[]{ "default-color" } }
+        };
+
+        public override Dictionary<string, Action<ClickableImage, string>> Setters => new Dictionary<string, Action<ClickableImage, string>>()
+        {
+            { "highlightColor", new Action<ClickableImage, string>((image, color) => image.highlightColor = GetColor(color)) },
+            { "defaultColor", new Action<ClickableImage, string>((image, color) => image.defaultColor = GetColor(color)) }
         };
 
         public override void HandleType(ComponentTypeWithData componentType, BSMLParserParams parserParams)
@@ -36,6 +45,25 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
                     parserParams.EmitEvent(clickEvent);
                 };
             }
+
+            if (componentType.data.TryGetValue("defaultColor", out var defaultColor))
+            {
+                clickableImage.defaultColor = GetColor(defaultColor);
+                clickableImage.color = clickableImage.defaultColor;
+            }
+
+            if (componentType.data.TryGetValue("highlightColor", out var highlightColor))
+            {
+                clickableImage.highlightColor = GetColor(highlightColor);
+            }
+        }
+
+        private static Color GetColor(string colorStr)
+        {
+            if (ColorUtility.TryParseHtmlString(colorStr, out Color color))
+                return color;
+            Logger.log?.Warn($"Color {colorStr}, is not a valid color.");
+            return Color.white;
         }
     }
 }
--- BeatSaberMarkupLanguage/TypeHandlers/ClickableTextHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/ClickableTextHandler.cs
@@ -2,17 +2,27 @@
 using BeatSaberMarkupLanguage.Parser;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using static BeatSaberMarkupLanguage.BSMLParser;
 
 namespace BeatSaberMarkupLanguage.TypeHandlers
 {
     [ComponentHandler(typeof(ClickableText))]
-    public class ClickableTextHandler : TypeHandler
+    public class ClickableTextHandler : TypeHandler<ClickableText>
     {
         public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
         {
             { "onClick", new[]{"on-click"} },
-            { "clickEvent", new[]{"click-event", "event-click"} }
+            { "clickEvent", new[]{"click-event", "event-click"} },
+            { "highlightColor", new[]{ "highlight-color" } },
+            { "defaultColor", new[]{ "default-color" } }
+        };
+
+        public override Dictionary<string, Action<ClickableText, string>> Setters => new Dictionary<string, Action<ClickableText, string>>()
+        {
+            { "highlightColor", new Action<ClickableText, string>((image, color) => image.highlightColor = GetColor(color)) },
+            { "defaultColor", new Action<ClickableText, string>((image, color) => image.defaultColor = GetColor(color)) }
+
         };
 
         public override void HandleType(ComponentTypeWithData componentType, BSMLParserParams parserParams)
@@ -36,6 +46,25 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
                     parserParams.EmitEvent(clickEvent);
                 };
             }
+
+            if (componentType.data.TryGetValue("defaultColor", out var defaultColor))
+            {
+                clickableText.defaultColor = GetColor(defaultColor);
+                clickableText.color = clickableText.defaultColor;
+            }
+
+            if (componentType.data.TryGetValue("highlightColor", out var highlightColor))
+            {
+                clickableText.highlightColor = GetColor(highlightColor);
+            }
+        }
+
+        private static Color GetColor(string colorStr)
+        {
+            if (ColorUtility.TryParseHtmlString(colorStr, out Color color))
+                return color;
+            Logger.log?.Warn($"Color {colorStr}, is not a valid color.");
+            return Color.white;
         }
     }
 }
