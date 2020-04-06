9890f55c4670d842b4d28fd85a9847aada7d022b
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-04-06 17:22:29 -0500

    Added AutomaticViewController with ability to specify information via attributes

---
+++ BeatSaberMarkupLanguage/Attributes/HotReloadAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using IPA.Utilities;
+
+namespace BeatSaberMarkupLanguage.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [Conditional("DEBUG"), Conditional("USE_HOT_RELOAD")]
+    public sealed class HotReloadAttribute : Attribute
+    {
+        public string GivenPath { get; }
+        public string[][] Aliases { get; set; }
+
+        private string _path = null;
+        public string Path
+        {
+            get
+            {
+                if (_path == null)
+                {
+                    foreach (var alias in Aliases)
+                    {
+                        if (alias.Length < 2) continue;
+                        if (GivenPath.StartsWith(alias[0]))
+                        {
+                            _path = alias[1] + GivenPath.Substring(alias[0].Length);
+                            break;
+                        }
+                    }
+                }
+                return _path;
+            }
+        }
+
+        public HotReloadAttribute([CallerFilePath] string basePath = null)
+            => GivenPath = basePath;
+    }
+}
---
+++ BeatSaberMarkupLanguage/ViewControllers/BSMLAutomaticViewController.cs
@@ -0,0 +1,44 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatSaberMarkupLanguage.ViewControllers
+{
+    public abstract class BSMLAutomaticViewController : BSMLViewController
+    {
+        private static string GetDefaultResourceName(Type type)
+        {
+            var ns = type.Namespace;
+            var name = type.Name;
+            return (ns.Length > 0 ? ns + "." : "") + name + ".bsml";
+        }
+
+        private string _resourceName;
+        public override string Content 
+        {
+            get
+            {
+                if (_resourceName == null)
+                {
+                    var viewDef = GetType().GetCustomAttribute<ViewDefinitionAttribute>();
+                    if (viewDef != null) _resourceName = viewDef.Definition;
+                    else _resourceName = GetDefaultResourceName(GetType());
+                }
+                return Utilities.GetResourceContent(GetType().Assembly, _resourceName);
+            } 
+        }
+
+        private readonly string hotReloadFrom;
+        public BSMLAutomaticViewController() : base()
+        {
+            var hotReloadAttr = GetType().GetCustomAttribute<HotReloadAttribute>();
+            if (hotReloadAttr == null) hotReloadFrom = null;
+            else hotReloadFrom = Path.ChangeExtension(hotReloadAttr.Path, ".bsml");
+        }
+    }
+}
