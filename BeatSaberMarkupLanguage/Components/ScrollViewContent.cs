29d78a76b8fa9424b1663f3672896d6a5f04e849
dcljr <dcljr@me.com>
2020-04-28 12:15:01 -0700

    Made ScrollView properly update when it's size changes

--- BeatSaberMarkupLanguage/Components/ScrollViewContent.cs
+++ BeatSaberMarkupLanguage/Components/ScrollViewContent.cs
@@ -1,7 +1,5 @@
 ﻿using HMUI;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace BeatSaberMarkupLanguage.Components
 {
@@ -9,17 +7,10 @@ namespace BeatSaberMarkupLanguage.Components
     {
         public ScrollView scrollView;
 
-        private void Start()
+        void OnRectTransformDimensionsChange()
         {
-            LayoutRebuilder.ForceRebuildLayoutImmediate(transform as RectTransform);
-            StartCoroutine(SetupScrollView());
-        }
-        private IEnumerator SetupScrollView()
-        {
-            RectTransform rectTransform = (transform as RectTransform);
-            yield return new WaitWhile(() => rectTransform.sizeDelta.y == -1); //This is a reliable way to wait for the vertical layout group to be set up which it must be before the scrollview can be setup
-            scrollView.Setup();
-            scrollView.RefreshButtonsInteractibility();
+            scrollView?.Setup();
+            scrollView?.RefreshButtonsInteractibility();
         }
     }
 }
