9fe9bcfdeefc54dd2e6c300040f57a876fa50b98
Zingabopp <Zingabopp@gmail.com>
2020-09-15 16:29:49 -0500

    Added UnityEvent properties to DragHelper

--- BeatSaberMarkupLanguage/Components/Settings/DragHelper.cs
+++ BeatSaberMarkupLanguage/Components/Settings/DragHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace BeatSaberMarkupLanguage.Components.Settings
@@ -12,9 +13,39 @@ namespace BeatSaberMarkupLanguage.Components.Settings
     {
         public event EventHandler<PointerEventData> DragStarted;
         public event EventHandler<PointerEventData> DragReleased;
+        private DragStartedEvent _dragStartedEvent = new DragStartedEvent();
+        private DragReleasedEvent _dragReleasedEvent = new DragReleasedEvent();
 
-        public void OnBeginDrag(PointerEventData eventData) => DragStarted?.Invoke(this, eventData);
+        public DragStartedEvent onDragStarted
+        {
+            get { return _dragStartedEvent; }
+            set { _dragStartedEvent = value; }
+        }
+        public DragReleasedEvent onDragReleased
+        {
+            get { return _dragReleasedEvent; }
+            set { _dragReleasedEvent = value; }
+        }
 
-        public void OnEndDrag(PointerEventData eventData) => DragReleased?.Invoke(this, eventData);
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            DragStarted.RaiseEventSafe(this, eventData, nameof(DragStarted));
+            onDragStarted?.Invoke();
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            DragReleased.RaiseEventSafe(this, eventData, nameof(DragReleased));
+            onDragReleased?.Invoke();
+        }
+
+        public class DragStartedEvent : UnityEvent
+        {
+
+        }
+        public class DragReleasedEvent : UnityEvent
+        {
+
+        }
     }
 }
