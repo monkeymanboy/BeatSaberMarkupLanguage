0020980de7f21bf9490092c0a8a3de3ce7e14e0a
Zingabopp <Zingabopp@gmail.com>
2020-09-15 15:16:15 -0500

    Added `DragStarted` and `DragReleased` events to `GenericSliderSetting`

---
+++ BeatSaberMarkupLanguage/Components/Settings/DragHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BeatSaberMarkupLanguage.Components.Settings
+{
+    public class DragHelper : MonoBehaviour, IEndDragHandler, IBeginDragHandler
+    {
+        public event EventHandler<PointerEventData> DragStarted;
+        public event EventHandler<PointerEventData> DragReleased;
+
+        public void OnBeginDrag(PointerEventData eventData) => DragStarted?.Invoke(this, eventData);
+
+        public void OnEndDrag(PointerEventData eventData) => DragReleased?.Invoke(this, eventData);
+    }
+}
--- BeatSaberMarkupLanguage/Components/Settings/GenericSliderSetting.cs
+++ BeatSaberMarkupLanguage/Components/Settings/GenericSliderSetting.cs
@@ -1,13 +1,43 @@
 ﻿using HMUI;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace BeatSaberMarkupLanguage.Components.Settings
 {
     public abstract class GenericSliderSetting : GenericInteractableSetting
     {
-        public RangeValuesTextSlider slider;
+        private RangeValuesTextSlider _slider;
+        public RangeValuesTextSlider slider
+        {
+            get
+            {
+                return _slider;
+            }
+            set
+            {
+                if (dragHelper != null)
+                {
+                    dragHelper.DragReleased -= OnDragReleased;
+                    dragHelper.DragStarted -= OnDragStarted;
+                    Destroy(dragHelper);
+                }
+                if(value != null)
+                {
+                    dragHelper = value.gameObject.AddComponent<DragHelper>();
+                    dragHelper.DragReleased += OnDragReleased;
+                    dragHelper.DragStarted += OnDragStarted;
+                }
+                _slider = value;
+            }
+        }
+
+        public event EventHandler<PointerEventData> DragStarted;
+        public event EventHandler<PointerEventData> DragReleased;
+
         protected TextMeshProUGUI text;
+        protected DragHelper dragHelper;
         public override bool interactable 
         { 
             get => slider?.interactable ?? false;
@@ -19,5 +49,7 @@ namespace BeatSaberMarkupLanguage.Components.Settings
                 }
             }
         }
+        private void OnDragStarted(object s, PointerEventData e) => DragStarted?.Invoke(this, e);
+        private void OnDragReleased(object s, PointerEventData e) => DragReleased?.Invoke(this, e);
     }
 }
