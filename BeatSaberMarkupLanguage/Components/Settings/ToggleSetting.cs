dc0a867af2cca9657fa9892b0c03809e1879fd04
Nicolas Gnyra <nicolas@gnyra.com>
2020-10-14 12:28:31 -0400

    Create toggle setting

---
+++ BeatSaberMarkupLanguage/Components/Settings/ToggleSetting.cs
@@ -0,0 +1,73 @@
+﻿using TMPro;
+using UnityEngine.UI;
+
+namespace BeatSaberMarkupLanguage.Components.Settings
+{
+    public class ToggleSetting : GenericInteractableSetting
+    {
+        public Toggle toggle;
+        public TextMeshProUGUI text;
+
+        private bool currentValue;
+
+        public bool Value
+        {
+            get => currentValue;
+            set
+            {
+                currentValue = value;
+                toggle.isOn = value;
+            }
+        }
+
+        public string Text
+        {
+            get => text.text;
+            set => text.text = value;
+        }
+
+        public override bool interactable
+        {
+            get => toggle.interactable;
+            set => toggle.interactable = value;
+        }
+
+        public override void Setup()
+        {
+            ReceiveValue();
+        }
+
+        public override void ApplyValue()
+        {
+            if (associatedValue != null)
+                associatedValue.SetValue(Value);
+        }
+
+        public override void ReceiveValue()
+        {
+            if (associatedValue != null)
+                Value = (bool)associatedValue.GetValue();
+        }
+
+        private void OnEnable()
+        {
+            toggle.onValueChanged.AddListener(OnValueChanged);
+            toggle.isOn = currentValue;
+        }
+
+        private void OnDisable()
+        {
+            toggle.onValueChanged.RemoveListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(bool value)
+        {
+            Value = value;
+
+            onChange?.Invoke(Value);
+
+            if (updateOnChange)
+                ApplyValue();
+        }
+    }
+}
---
+++ BeatSaberMarkupLanguage/Tags/Settings/ToggleSettingTag.cs
@@ -0,0 +1,46 @@
+﻿using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Components.Settings;
+using Polyglot;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatSaberMarkupLanguage.Tags
+{
+    public class ToggleSettingTag : BSMLTag
+    {
+        public override string[] Aliases => new[] { "toggle-setting" };
+        public virtual string PrefabToggleName => "Fullscreen";
+
+        public override GameObject CreateObject(Transform parent)
+        {
+            GameObject gameObject = Object.Instantiate(Resources.FindObjectsOfTypeAll<Toggle>().Select(x => x.transform.parent.gameObject).First(p => p.name == PrefabToggleName), parent, false);
+            GameObject nameText = gameObject.transform.Find("NameText").gameObject;
+
+            gameObject.name = "BSMLToggle";
+            gameObject.SetActive(false);
+
+            Object.Destroy(nameText.GetComponent<LocalizedTextMeshProUGUI>());
+
+            ToggleSetting toggleSetting = gameObject.AddComponent<ToggleSetting>();
+
+            toggleSetting.toggle = gameObject.GetComponentInChildren<Toggle>();
+            toggleSetting.toggle.interactable = true;
+            toggleSetting.toggle.onValueChanged.RemoveAllListeners();
+
+            toggleSetting.text = nameText.GetComponent<TextMeshProUGUI>();
+            toggleSetting.text.text = "Default Text";
+            toggleSetting.text.richText = true;
+            toggleSetting.text.overflowMode = TextOverflowModes.Ellipsis;
+
+            gameObject.AddComponent<ExternalComponents>().components.Add(toggleSetting.text);
+
+            gameObject.GetComponent<LayoutElement>().preferredWidth = 90;
+
+            gameObject.SetActive(true);
+
+            return gameObject;
+        }
+    }
+}
