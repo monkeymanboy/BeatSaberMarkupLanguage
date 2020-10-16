cd5f7a1b674a93b465354b7c7cc0ad1cdbe1c4a9
Caeden117 <caeden.s@outlook.com>
2020-10-16 15:43:25 -0700

    Cleanup unused namespaces

--- BeatSaberMarkupLanguage/Components/BSMLScrollableContainer.cs
+++ BeatSaberMarkupLanguage/Components/BSMLScrollableContainer.cs
@@ -1,10 +1,5 @@
 ﻿using HMUI;
-using IPA.Utilities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
