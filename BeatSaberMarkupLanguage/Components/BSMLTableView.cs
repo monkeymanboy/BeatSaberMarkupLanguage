c7ce0f42ae83a0d4d5a2505316ce5aa112181a59
Caeden117 <caeden.s@outlook.com>
2021-03-14 22:41:46 -0700

    Add Harmony Patches to fix base game bugs, tweak values

--- BeatSaberMarkupLanguage/Components/BSMLTableView.cs
+++ BeatSaberMarkupLanguage/Components/BSMLTableView.cs
@@ -7,11 +7,10 @@ namespace BeatSaberMarkupLanguage.Components
         public override void ReloadData()
         {
             base.ReloadData();
-            if(tableType == TableType.Horizontal)
+            if (tableType == TableType.Horizontal)
             {
                 contentTransform.anchorMin = new UnityEngine.Vector2(0, 0);
-                contentTransform.anchorMax = new UnityEngine.Vector2(1, 1);
-                contentTransform.sizeDelta = new UnityEngine.Vector2(0, 0);
+                contentTransform.anchorMax = new UnityEngine.Vector2(0, 1);
             }
         }
     }
