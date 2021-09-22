89a29b05e48e63b989de30b961cddca07142b952
Kinsi <kinsi55@users.noreply.github.com>
2021-09-23 01:05:42 +0200

    Create FixModalLayer.cs

---
+++ BeatSaberMarkupLanguage/Harmony Patches/FixModalLayer.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using HarmonyLib;
+using HMUI;
+using UnityEngine;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches {
+	[HarmonyPatch(typeof(ModalView), nameof(ModalView.Show))]
+	public static class FixModalBlockerLayer {
+		static void Postfix(ModalView __instance, GameObject ____blockerGO) {
+			var cb = ____blockerGO.GetComponent<Canvas>();
+
+			var h = (__instance.transform.parent.GetComponentInParent<HMUI.Screen>()
+				?.GetComponentsInChildren<Canvas>()
+				.Where(x => x.sortingLayerID == cb.sortingLayerID)
+				.Select(x => x.sortingOrder)
+				.DefaultIfEmpty(0)
+				.Max() ?? 0) + 1;
+
+			cb.overrideSorting = true;
+			cb.sortingOrder = h;
+
+			cb = __instance.GetComponent<Canvas>();
+			cb.overrideSorting = true;
+			cb.sortingOrder = h + 1;
+		}
+	}
+}
