34b25a7d2f36a281e39f455e4fd436469c76db91
PixelBoom <rithikbhatia12@gmail.com>
2021-12-27 04:56:11 +0400

    Added GameplaySetup Hiding to the UI

--- BeatSaberMarkupLanguage/GameplaySetup/GameplaySetup.cs
+++ BeatSaberMarkupLanguage/GameplaySetup/GameplaySetup.cs
@@ -1,5 +1,6 @@
 ﻿using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Components;
+using HMUI;
 using IPA.Utilities;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using UnityEngine.UI;
 
 namespace BeatSaberMarkupLanguage.GameplaySetup
 {
-    public class GameplaySetup : PersistentSingleton<GameplaySetup>
+    public class GameplaySetup : PersistentSingleton<GameplaySetup>, TableView.IDataSource
     {
         public event Action TabsCreatedEvent;
 
@@ -26,6 +27,12 @@ namespace BeatSaberMarkupLanguage.GameplaySetup
 
         [UIComponent("mods-tab")]
         private Transform modsTab;
+
+        [UIComponent("list-modal")]
+        private ModalView listModal;
+
+        [UIComponent("mods-list")]
+        private CustomListTableData modsList;
 
         [UIValue("vanilla-items")]
         private List<Transform> vanillaItems = new List<Transform>();
@@ -47,9 +54,11 @@ namespace BeatSaberMarkupLanguage.GameplaySetup
             textSegmentedControl.sizeDelta = new Vector2(0, 6);
             layoutGroup = textSegmentedControl.GetComponent<LayoutGroup>();
             BSMLParser.instance.Parse(Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "BeatSaberMarkupLanguage.Views.gameplay-setup.bsml"), gameplaySetupViewController.gameObject, this);
-            
+
+            modsList.tableView.SetDataSource(this, false);
             gameplaySetupViewController.didActivateEvent += GameplaySetupDidActivate;
             gameplaySetupViewController.didDeactivateEvent += GameplaySetupDidDeactivate;
+            listModal.blockerClickedEvent += ClickedOffModal;
         }
 
         private void GameplaySetupDidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
@@ -75,6 +84,7 @@ namespace BeatSaberMarkupLanguage.GameplaySetup
             foreach (GameplaySetupMenu menu in menus)
                 menu.SetVisible(menu.IsMenuType(menuType));
 
+            modsList.tableView.ReloadData();
             TabsCreatedEvent?.Invoke();
         }
 
@@ -83,6 +93,20 @@ namespace BeatSaberMarkupLanguage.GameplaySetup
             tabSelector.textSegmentedControl.SelectCellWithNumber(0);
             vanillaTab.gameObject.SetActive(true);
             modsTab.gameObject.SetActive(false);
+        }
+
+        private void ClickedOffModal()
+        {
+            GameplaySetupDidActivate(false, false, false);
+        }
+
+        [UIAction("show-modal")]
+        private void UpdateTable()
+        {
+            listModal.Show(true, true, () =>
+            {
+                modsList.tableView.ReloadDataKeepingPosition();
+            });
         }
 
         public void AddTab(string name, string resource, object host)
@@ -120,5 +144,37 @@ namespace BeatSaberMarkupLanguage.GameplaySetup
             if (menu.Count() > 0)
                 menus.Remove(menu.FirstOrDefault());
         }
+
+        #region Data Source
+
+        public const string ReuseIdentifier = "GameplaySetupCell";
+        private GameplaySetupCell GetCell()
+        {
+            TableCell tableCell = modsList.tableView.DequeueReusableCellForIdentifier(ReuseIdentifier);
+
+            if (tableCell == null)
+            {
+                tableCell = new GameObject(nameof(GameplaySetupCell)).AddComponent<GameplaySetupCell>();
+                tableCell.interactable = true;
+
+                tableCell.reuseIdentifier = ReuseIdentifier;
+                BSMLParser.instance.Parse(
+                BeatSaberMarkupLanguage.Utilities.GetResourceContent(
+                    Assembly.GetExecutingAssembly(),
+                    "BeatSaberMarkupLanguage.Views.gameplay-setup-cell.bsml"),
+                tableCell.gameObject,
+                tableCell);
+            }
+
+            return (GameplaySetupCell)tableCell;
+        }
+
+        public float CellSize() => 8f;
+
+        public int NumberOfCells() => menus.Count;
+
+        public TableCell CellForIdx(TableView tableView, int idx) => GetCell().PopulateCell((GameplaySetupMenu)menus[idx]);
+
+        #endregion
     }
 }
---
+++ BeatSaberMarkupLanguage/GameplaySetup/GameplaySetupCell.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using BeatSaberMarkupLanguage.Attributes;
+using HMUI;
+
+namespace BeatSaberMarkupLanguage.GameplaySetup
+{
+    internal class GameplaySetupCell : TableCell, INotifyPropertyChanged
+    {
+        private GameplaySetupMenu tab;
+
+        [UIValue("tab-name")]
+        private string Name => tab?.name;
+
+        [UIValue("tab-visible")]
+        private bool Visible
+        {
+            get => tab != null && tab.visible;
+            set
+            {
+                if (tab != null)
+                {
+                    tab.visible = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Visible)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public GameplaySetupCell PopulateCell(GameplaySetupMenu tab)
+        {
+            this.tab = tab;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Visible)));
+            return this;
+        }
+    }
+}
