7d55c3a6c51ee21c1b6fafebaf1fad71a0bf85a7
Sirspam <71392316+Sirspam@users.noreply.github.com>
2022-02-27 17:05:32 +0000

    Added click noise to Slider Settings

---
+++ BeatSaberMarkupLanguage/Harmony Patches/RangeValuesTextSliderValueDidChangePatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using HarmonyLib;
+using HMUI;
+using UnityEngine;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches
+{
+	[HarmonyPatch(typeof(RangeValuesTextSlider), nameof(RangeValuesTextSlider.HandleNormalizedValueDidChange), MethodType.Normal)]
+	public class RangeValuesTextSliderValueDidChangePatch
+	{
+		private static BasicUIAudioManager _basicUIAudioManager;
+		
+		public static void Postfix()
+		{
+			if (_basicUIAudioManager == null)
+			{
+				_basicUIAudioManager = Resources.FindObjectsOfTypeAll<BasicUIAudioManager>().First();
+			}
+			_basicUIAudioManager.HandleButtonClickEvent();
+		}
+	}
+}
