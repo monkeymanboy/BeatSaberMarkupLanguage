58a06f4ee7deb301d531f62fa468daaa87e473f4
Zephyr <67527746+zeph-yr@users.noreply.github.com>
2023-05-18 02:29:25 +0800

    Disable thumbstick scrolling in bsml tabs

---
+++ BeatSaberMarkupLanguage/Harmony Patches/ScrollViewHandlePointerDidEnterPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using HMUI;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches
+{
+    // Feature to disable thumbstick scrolling in BSMLTabs for users suffering from thumbstick drift
+    // who just want to set sliders without them scrolling away
+
+    [HarmonyPatch(typeof(ScrollView), "HandlePointerDidEnter")]
+    class ScrollViewHandlePointerDidEnterPatch
+    {
+        static void Postfix(ScrollView __instance)
+        {
+            if (__instance.GetComponentInParent<GameplaySetupViewController>() != null && Plugin.config.DisableThumbstickScroll)
+            {
+                Traverse.Create(__instance).Field("_isHoveredByPointer").SetValue(false);
+            }
+        }
+    }
+}
