061ae77e00a82d125011435a201e1a8f2dbbb4b8
Zephyr <67527746+zeph-yr@users.noreply.github.com>
2023-05-19 08:22:39 +0800

    Feature: Option to limit control of scrolling in Left Menu to scrollbars only (#141)
    Disable thumbstick scrolling in bsml tabs

---
+++ BeatSaberMarkupLanguage/Harmony Patches/ScrollViewHandlePointerDidEnterPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using HMUI;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches
+{
+    // Feature to disable thumbstick scrolling in BSMLTabs for users suffering from thumbstick drift
+    // who just want to set sliders without them scrolling away
+
+    [HarmonyPatch(typeof(ScrollView), "HandlePointerDidEnter")]
+    class ScrollViewHandlePointerDidEnterPatch
+    {
+        static void Postfix(ScrollView __instance)
+        {
+            if (Plugin.config.DisableThumbstickScroll && __instance.GetComponentInParent<GameplaySetupViewController>() != null)
+            {
+                Traverse.Create(__instance).Field("_isHoveredByPointer").SetValue(false);
+            }
+        }
+    }
+}
