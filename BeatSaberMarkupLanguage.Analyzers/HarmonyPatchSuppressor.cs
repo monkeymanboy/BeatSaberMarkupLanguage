13bc02548a0cfb4e64fb856cf87930b9ca14b6de
Nicolas Gnyra <nicolas@gnyra.com>
2023-05-30 20:04:10 -0400

    Add some simple DiagnosticSuppressors

---
+++ BeatSaberMarkupLanguage.Analyzers/HarmonyPatchSuppressor.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using BeatSaberMarkupLanguage.Analyzers.Resources;
+using BeatSaberMarkupLanguage.Analyzers.Utilities;
+using HarmonyLib;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace BeatSaberMarkupLanguage.Analyzers;
+
+/// <summary>
+/// A <see cref="DiagnosticSuppressor"/> for Harmony patch methods and associated parameters.
+/// </summary>
+// TODO: This really belongs in its own project/package. Nothing currently available on NuGet seems to do this, so I'm leaving it here for now.
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class HarmonyPatchSuppressor : DiagnosticSuppressor
+{
+    private static readonly SuppressionDescriptor RemoveUnusedPrivateMemberRule = new(
+        id: "BSMLS0007",
+        suppressedDiagnosticId: "IDE0051",
+        justification: Strings.RemoveUnusedPrivateMemberHarmonyPatchSuppressorJustification);
+
+    private static readonly SuppressionDescriptor PrivateMemberNeverUsedRule = new(
+        id: "BSMLS0008",
+        suppressedDiagnosticId: "CS0169",
+        justification: Strings.PrivateMemberNeverUsedHarmonyPatchSuppressorJustification);
+
+    private static readonly SuppressionDescriptor StyleCopParameterNamingRule = new(
+        id: "BSMLS0009",
+        suppressedDiagnosticId: "SA1313",
+        justification: Strings.StyleCopParameterNamingHarmonyPatchSuppressorJustification);
+
+    private static readonly string[] HarmonyDefaultMethodNames = new[] { "Cleanup", "Finalizer", "Postfix", "Prefix", "Prepare", "TargetMethod", "TargetMethods", "Transpiler" };
+
+    /// <inheritdoc />
+    public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions => ImmutableArray.Create(
+        RemoveUnusedPrivateMemberRule,
+        PrivateMemberNeverUsedRule,
+        StyleCopParameterNamingRule);
+
+    /// <inheritdoc />
+    public override void ReportSuppressions(SuppressionAnalysisContext context)
+    {
+        foreach (var diagnostic in context.ReportedDiagnostics)
+        {
+            this.AnalyzeDiagnosticField(diagnostic, context);
+        }
+    }
+
+    private static bool IsSuppressable(ISymbol symbol)
+    {
+        if (!symbol.ContainingType.GetAttributes().Any(a => a.AttributeClass != null && a.AttributeClass.Matches(typeof(HarmonyPatch))))
+        {
+            return false;
+        }
+
+        if (symbol is IMethodSymbol &&
+            !HarmonyDefaultMethodNames.Contains(symbol.Name) &&
+            !symbol.GetAttributes().Any(a => a.AttributeClass != null && a.AttributeClass.Matches(
+                typeof(HarmonyCleanup),
+                typeof(HarmonyFinalizer),
+                typeof(HarmonyPostfix),
+                typeof(HarmonyPrefix),
+                typeof(HarmonyPrepare),
+                typeof(HarmonyReversePatch),
+                typeof(HarmonyTargetMethod),
+                typeof(HarmonyTargetMethods),
+                typeof(HarmonyTranspiler))))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void AnalyzeDiagnosticField(Diagnostic diagnostic, SuppressionAnalysisContext context)
+    {
+        var fieldDeclarationSyntax = context.GetSuppressibleNode<SyntaxNode>(diagnostic, n => n is MethodDeclarationSyntax or ParameterSyntax);
+        if (fieldDeclarationSyntax == null)
+        {
+            return;
+        }
+
+        var syntaxTree = diagnostic.Location.SourceTree;
+        if (syntaxTree == null)
+        {
+            return;
+        }
+
+        var model = context.GetSemanticModel(syntaxTree);
+        var declaredSymbol = model.GetDeclaredSymbol(fieldDeclarationSyntax)!;
+        if (!IsSuppressable(declaredSymbol))
+        {
+            return;
+        }
+
+        foreach (var descriptor in this.SupportedSuppressions.Where(d => d.SuppressedDiagnosticId == diagnostic.Id))
+        {
+            context.ReportSuppression(Suppression.Create(descriptor, diagnostic));
+        }
+    }
+}
---
+++ BeatSaberMarkupLanguage.Analyzers/UIAttributeSuppressor.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using BeatSaberMarkupLanguage.Analyzers.Resources;
+using BeatSaberMarkupLanguage.Analyzers.Utilities;
+using BeatSaberMarkupLanguage.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace BeatSaberMarkupLanguage.Analyzers;
+
+/// <summary>
+/// A <see cref="DiagnosticSuppressor"/> for properties and fields marked with various BSML attributes.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class UIAttributeSuppressor : DiagnosticSuppressor
+{
+    private static readonly SuppressionDescriptor AddReadonlyModifierRule = new(
+        id: "BSMLS0001",
+        suppressedDiagnosticId: "IDE0044",
+        justification: Strings.AddReadonlyModifierUIAttributeSuppressorJustification);
+
+    private static readonly SuppressionDescriptor RemoveUnusedPrivateMemberRule = new(
+        id: "BSMLS0002",
+        suppressedDiagnosticId: "IDE0051",
+        justification: Strings.RemoveUnusedPrivateMemberUIAttributeSuppressorJustification);
+
+    private static readonly SuppressionDescriptor RemoveUnreadPrivateMemberRule = new(
+        id: "BSMLS0003",
+        suppressedDiagnosticId: "IDE0052",
+        justification: Strings.RemoveUnreadPrivateMemberUIAttributeSuppressorJustification);
+
+    private static readonly SuppressionDescriptor PrivateMemberNeverUsedRule = new(
+        id: "BSMLS0004",
+        suppressedDiagnosticId: "CS0169",
+        justification: Strings.PrivateMemberNeverUsedUIAttributeSuppressorJustification);
+
+    private static readonly SuppressionDescriptor PrivateFieldAssignedButNeverUsedRule = new(
+        id: "BSMLS0005",
+        suppressedDiagnosticId: "CS0414",
+        justification: Strings.PrivateFieldAssignedButNeverUsedUIAttributeSuppressorJustification);
+
+    private static readonly SuppressionDescriptor FieldNeverAssignedRule = new(
+        id: "BSMLS0006",
+        suppressedDiagnosticId: "CS0649",
+        justification: Strings.FieldNeverAssignedUIAttributeSuppressorJustification);
+
+    /// <inheritdoc />
+    public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions => ImmutableArray.Create(
+        AddReadonlyModifierRule,
+        RemoveUnusedPrivateMemberRule,
+        RemoveUnreadPrivateMemberRule,
+        PrivateMemberNeverUsedRule,
+        PrivateFieldAssignedButNeverUsedRule,
+        FieldNeverAssignedRule);
+
+    /// <inheritdoc />
+    public override void ReportSuppressions(SuppressionAnalysisContext context)
+    {
+        foreach (var diagnostic in context.ReportedDiagnostics)
+        {
+            this.AnalyzeDiagnosticField(diagnostic, context);
+        }
+    }
+
+    private static bool IsSuppressable(ISymbol fieldSymbol)
+    {
+        if (fieldSymbol.GetAttributes().Any(a => a.AttributeClass != null && a.AttributeClass.Matches(typeof(UIAction), typeof(UIComponent), typeof(UIObject), typeof(UIParams), typeof(UIValue))))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void AnalyzeDiagnosticField(Diagnostic diagnostic, SuppressionAnalysisContext context)
+    {
+        var fieldDeclarationSyntax = context.GetSuppressibleNode<SyntaxNode>(diagnostic, n => n is VariableDeclaratorSyntax or MethodDeclarationSyntax or PropertyDeclarationSyntax);
+        if (fieldDeclarationSyntax == null)
+        {
+            return;
+        }
+
+        var syntaxTree = diagnostic.Location.SourceTree;
+        if (syntaxTree == null)
+        {
+            return;
+        }
+
+        var model = context.GetSemanticModel(syntaxTree);
+        var declaredSymbol = model.GetDeclaredSymbol(fieldDeclarationSyntax)!;
+        if (!IsSuppressable(declaredSymbol))
+        {
+            return;
+        }
+
+        foreach (var descriptor in this.SupportedSuppressions.Where(d => d.SuppressedDiagnosticId == diagnostic.Id))
+        {
+            context.ReportSuppression(Suppression.Create(descriptor, diagnostic));
+        }
+    }
+}
