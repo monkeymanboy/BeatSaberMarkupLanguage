3c8e9d412806f730f9aef2a594cd4e827f89eea4
Nicolas Gnyra <nicolas@gnyra.com>
2023-05-30 20:11:50 -0400

    Clean up excessively verbose dispose pattern stuff

--- BeatSaberMarkupLanguage/OpenType/OpenTypeFont.cs
+++ BeatSaberMarkupLanguage/OpenType/OpenTypeFont.cs
@@ -94,7 +94,7 @@ namespace BeatSaberMarkupLanguage.OpenType
 
         public IEnumerable<TableRecord> Tables => tables;
 
-        private bool disposedValue = false; // To detect redundant calls
+        private bool disposedValue = false;
 
         protected virtual void Dispose(bool disposing)
         {
@@ -105,27 +105,10 @@ namespace BeatSaberMarkupLanguage.OpenType
                     Reader?.Dispose();
                 }
 
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~OpenTypeFont()
-        // {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
+        public void Dispose() => Dispose(true);
     }
 }
