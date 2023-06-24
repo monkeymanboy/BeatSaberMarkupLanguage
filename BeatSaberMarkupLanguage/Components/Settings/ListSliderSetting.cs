monkeymanboy/BeatSaberMarkupLanguage

commit 10297ab8f68e2085adb82a4a6bfa1c64a1397a0d

Fix SliderSetting text being wonky if value is past halfway

BeatSaberMarkupLanguage/Components/Settings/ListSliderSetting.cs
@@ -29,7 +29,6 @@ namespace BeatSaberMarkupLanguage.Components.Settings
             slider.maxValue = values.Count() - 1;
             text = slider.GetComponentInChildren<TextMeshProUGUI>();
             slider.numberOfSteps = values.Count;
-            ReceiveValue();
             slider.valueDidChangeEvent += OnChange;
             StartCoroutine(SetInitialText());
         }
@@ -50,6 +49,11 @@ namespace BeatSaberMarkupLanguage.Components.Settings
         protected string TextForValue(object value)
         {
             return formatter == null ? value.ToString() : (formatter.Invoke(value) as string);
+        }
+
+        private void Awake()
+        {
+            ReceiveValue();
         }
 
         private void OnEnable()

BeatSaberMarkupLanguage/Components/Settings/SliderSetting.cs
@@ -26,7 +26,6 @@ namespace BeatSaberMarkupLanguage.Components.Settings
 
             text = slider.GetComponentInChildren<TextMeshProUGUI>();
             slider.numberOfSteps = (int)Math.Round((slider.maxValue - slider.minValue) / increments) + 1;
-            ReceiveValue();
             slider.valueDidChangeEvent += OnChange;
         }
 
@@ -63,6 +62,11 @@ namespace BeatSaberMarkupLanguage.Components.Settings
             {
                 return formatter == null ? value.ToString("N2") : (formatter.Invoke(value) as string);
             }
+        }
+
+        private void Awake()
+        {
+            ReceiveValue();
         }
 
         private void OnChange(TextSlider textSlider, float val)

commit aac5db9ba0a693111559d78bf78f51f8443a70c6

Fix slider settings outside Mod Settings not having correct initial text
Regression introduced in 10297ab

BeatSaberMarkupLanguage/Components/Settings/ListSliderSetting.cs
@@ -28,6 +28,12 @@ namespace BeatSaberMarkupLanguage.Components.Settings
             text = slider.GetComponentInChildren<TextMeshProUGUI>();
             slider.numberOfSteps = values.Count;
             slider.valueDidChangeEvent += OnChange;
+
+            // TextSlider.UpdateVisuals doesn't work properly when disabled
+            if (slider.gameObject.activeInHierarchy)
+            {
+                ReceiveValue();
+            }
         }
 
         public override void ApplyValue()
@@ -51,7 +57,6 @@ namespace BeatSaberMarkupLanguage.Components.Settings
         private void Awake()
         {
             ReceiveValue();
-            text.text = TextForValue(Value);
         }
 
         private void OnChange(TextSlider textSlider, float val)

BeatSaberMarkupLanguage/Components/Settings/SliderSetting.cs
@@ -27,6 +27,12 @@ namespace BeatSaberMarkupLanguage.Components.Settings
             text = slider.GetComponentInChildren<TextMeshProUGUI>();
             slider.numberOfSteps = (int)Math.Round((slider.maxValue - slider.minValue) / increments) + 1;
             slider.valueDidChangeEvent += OnChange;
+
+            // TextSlider.UpdateVisuals doesn't work properly when disabled
+            if (slider.gameObject.activeInHierarchy)
+            {
+                ReceiveValue();
+            }
         }
 
         public override void ApplyValue()

BeatSaberMarkupLanguage/ViewControllers/TestViewController.cs
@@ -27,6 +27,15 @@ namespace BeatSaberMarkupLanguage.ViewControllers
 
         public override string ResourceName => "BeatSaberMarkupLanguage.Views.test.bsml";
 
+        [UIValue("slider-value")]
+        public float SliderValue { get; set; } = 0.5f;
+
+        [UIValue("options")]
+        public List<object> Options { get; } = new() { "option1", "option2", "option3" };
+
+        [UIValue("list-slider-value")]
+        public string ListValue { get; set; } = "option2";
+
         [UIValue("header")]
         public string HeaderText
         {

BeatSaberMarkupLanguage/Views/test.bsml
@@ -1,6 +1,12 @@
 <bg xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='https://monkeymanboy.github.io/BSML-Docs/ https://raw.githubusercontent.com/monkeymanboy/BSML-Docs/gh-pages/BSMLSchema.xsd'>
   <macro.define id='button-text' value='Macro defined button text'></macro.define>
   <tab-selector tab-tag='new-tab'></tab-selector>
+  <tab tags='new-tab' tab-name='Settings tests'>
+    <settings-container>
+      <slider-setting min='-1' max='1' value='slider-value'/>
+      <list-slider-setting options='options' value='list-slider-value'/>
+    </settings-container>
+  </tab>
   <tab tags='new-tab' tab-name='ScrollView test'>
     <vertical vertical-fit='PreferredSize' pref-height='60' pref-width='80'>
       <text text='ScrollView test' />