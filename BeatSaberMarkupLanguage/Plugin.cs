aaffe12832af27025916ab351abf1f58745d565c
Nicolas Gnyra <nicolas@gnyra.com>
2023-07-03 18:28:40 -0400

    Simplify redundant task running

--- BeatSaberMarkupLanguage/Plugin.cs
+++ BeatSaberMarkupLanguage/Plugin.cs
@@ -7,7 +7,6 @@ using BeatSaberMarkupLanguage.Animations;
 using HarmonyLib;
 using IPA;
 using IPA.Config.Stores;
-using IPA.Utilities.Async;
 using TMPro;
 using Conf = IPA.Config.Config;
 using IPALogger = IPA.Logging.Logger;
@@ -44,42 +43,39 @@ namespace BeatSaberMarkupLanguage
         [OnStart]
         public void OnStart()
         {
-            Task.Run(async () =>
-            {
-                await FontManager.AsyncLoadSystemFonts();
-
-                await UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
-                {
-                    if (!FontManager.TryGetTMPFontByFullName("Segoe UI", out TMP_FontAsset fallback) &&
-                        !FontManager.TryGetTMPFontByFamily("Arial", out fallback))
-                    {
-                        Logger.Log.Error("Could not find fonts for either Segoe UI or Arial to set up fallbacks");
-                        return;
-                    }
-
-                    Logger.Log.Debug("Waiting for default font presence");
-
-                    while (BeatSaberUI.MainTextFont == null)
-                    {
-                        await Task.Yield();
-                    }
-
-                    Logger.Log.Debug("Setting up default font fallbacks");
-
-                    // remove built-in fallback fonts to avoid inconsistencies between CJK characters
-                    BeatSaberUI.MainTextFont.fallbackFontAssets.RemoveAll((asset) => FontNamesToRemove.Contains(asset.name));
-                    BeatSaberUI.MainTextFont.fallbackFontAssetTable.RemoveAll((asset) => FontNamesToRemove.Contains(asset.name));
-                    BeatSaberUI.MainTextFont.fallbackFontAssetTable.Add(fallback);
-                });
-            }).ContinueWith((task) => Logger.Log.Error($"Failed to set up fallback fonts\n{task.Exception}"), TaskContinuationOptions.OnlyOnFaulted);
-
-            UnityMainThreadTaskScheduler.Factory.StartNew(AnimationController.instance.InitializeLoadingAnimation)
-                .ContinueWith((task) => Logger.Log.Error($"Failed to initialize loading animation\n{task.Exception}"), TaskContinuationOptions.OnlyOnFaulted);
+            LoadAndSetUpFontFallbacks().ContinueWith((task) => Logger.Log.Error($"Failed to set up fallback fonts\n{task.Exception}"), TaskContinuationOptions.OnlyOnFaulted);
+            AnimationController.instance.InitializeLoadingAnimation().ContinueWith((task) => Logger.Log.Error($"Failed to initialize loading animation\n{task.Exception}"), TaskContinuationOptions.OnlyOnFaulted);
         }
 
         [OnExit]
         public void OnExit()
         {
+        }
+
+        private async Task LoadAndSetUpFontFallbacks()
+        {
+            await FontManager.AsyncLoadSystemFonts();
+
+            if (!FontManager.TryGetTMPFontByFullName("Segoe UI", out TMP_FontAsset fallback) &&
+                !FontManager.TryGetTMPFontByFamily("Arial", out fallback))
+            {
+                Logger.Log.Error("Could not find fonts for either Segoe UI or Arial to set up fallbacks");
+                return;
+            }
+
+            Logger.Log.Debug("Waiting for default font presence");
+
+            while (BeatSaberUI.MainTextFont == null)
+            {
+                await Task.Yield();
+            }
+
+            Logger.Log.Debug("Setting up default font fallbacks");
+
+            // remove built-in fallback fonts to avoid inconsistencies between CJK characters
+            BeatSaberUI.MainTextFont.fallbackFontAssets.RemoveAll((asset) => FontNamesToRemove.Contains(asset.name));
+            BeatSaberUI.MainTextFont.fallbackFontAssetTable.RemoveAll((asset) => FontNamesToRemove.Contains(asset.name));
+            BeatSaberUI.MainTextFont.fallbackFontAssetTable.Add(fallback);
         }
     }
 }
