653c6923a2f84e85e845dcf4eb614dba39ef3371
Nicolas Gnyra <nicolas@gnyra.com>
2023-07-03 20:29:12 -0400

    Add patch to increase level list song name text size slightly

---
+++ BeatSaberMarkupLanguage/Harmony Patches/LevelListTableCell.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches
+{
+    [HarmonyPatch(typeof(LevelListTableCell), nameof(LevelListTableCell.RefreshVisuals))]
+    internal static class LevelListTableCell_RefreshVisuals
+    {
+        private static void Postfix(LevelListTableCell __instance)
+        {
+            RectTransform rectTransform = __instance._songNameText.rectTransform;
+            Vector2 offsetMin = rectTransform.offsetMin;
+            Vector2 offsetMax = rectTransform.offsetMax;
+            offsetMax.y = offsetMin.y + 6; // by default sizeDelta.y is 5.74 which is _slightly_ too small
+            rectTransform.offsetMax = offsetMax;
+        }
+    }
+}
