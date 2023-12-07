6927d6ecaa4eda457a20842dbc7618295accabbc
Nicolas Gnyra <nicolas@gnyra.com>
2023-12-07 09:22:50 -0500

    Fix LevelListTableCell text rect again

--- BeatSaberMarkupLanguage/Harmony Patches/LevelListTableCell.cs
+++ BeatSaberMarkupLanguage/Harmony Patches/LevelListTableCell.cs
@@ -11,7 +11,7 @@ namespace BeatSaberMarkupLanguage.Harmony_Patches
             RectTransform rectTransform = __instance._songNameText.rectTransform;
             Vector2 offsetMin = rectTransform.offsetMin;
             Vector2 offsetMax = rectTransform.offsetMax;
-            offsetMax.y = offsetMin.y + 6; // by default sizeDelta.y is 5.74 which is _slightly_ too small
+            offsetMax.y = offsetMin.y + 6.5f; // by default sizeDelta.y is 5.74 which is _slightly_ too small
             rectTransform.offsetMax = offsetMax;
         }
     }
