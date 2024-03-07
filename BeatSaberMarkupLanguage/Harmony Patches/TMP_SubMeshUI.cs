53b01695f834ba5b629f787531ba60d1938dafe6
Nicolas Gnyra <nicolas@gnyra.com>
2024-03-07 17:45:53 -0500

    Fix UpdateMaterial exception at shutdown

---
+++ BeatSaberMarkupLanguage/Harmony Patches/TMP_SubMeshUI.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TMPro;
+
+namespace BeatSaberMarkupLanguage.Harmony_Patches
+{
+    [HarmonyPatch(typeof(TMP_SubMeshUI), "UpdateMaterial")]
+    internal class TMP_SubMeshUI_UpdateMaterial
+    {
+        private static bool Prefix(TMP_SubMeshUI __instance)
+        {
+            return __instance.textComponent.fontSharedMaterial != null;
+        }
+    }
+}
