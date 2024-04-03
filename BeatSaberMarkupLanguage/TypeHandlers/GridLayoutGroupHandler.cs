f76a58b00a75fa2f81ab34d0f92967af59108659
Nicolas Gnyra <nicolas@gnyra.com>
2024-04-03 19:41:47 -0400

    Add Vector2 cellSize and spacing

--- BeatSaberMarkupLanguage/TypeHandlers/GridLayoutGroupHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/GridLayoutGroupHandler.cs
@@ -10,16 +10,20 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
     {
         public override Dictionary<string, string[]> Props => new()
         {
+            { "cellSize", new[] { "cell-size" } },
             { "cellSizeX", new[] { "cell-size-x" } },
             { "cellSizeY", new[] { "cell-size-y" } },
+            { "spacing", new[] { "spacing" } },
             { "spacingX", new[] { "spacing-x" } },
             { "spacingY", new[] { "spacing-y" } },
         };
 
         public override Dictionary<string, Action<GridLayoutGroup, string>> Setters => new()
         {
+            { "cellSize", new Action<GridLayoutGroup, string>((component, value) => component.cellSize = Parse.Vector2(value)) },
             { "cellSizeX", new Action<GridLayoutGroup, string>((component, value) => component.cellSize = new Vector2(Parse.Float(value), component.cellSize.y)) },
             { "cellSizeY", new Action<GridLayoutGroup, string>((component, value) => component.cellSize = new Vector2(component.cellSize.x, Parse.Float(value))) },
+            { "spacing", new Action<GridLayoutGroup, string>((component, value) => component.spacing = Parse.Vector2(value)) },
             { "spacingX", new Action<GridLayoutGroup, string>((component, value) => component.spacing = new Vector2(Parse.Float(value), component.spacing.y)) },
             { "spacingY", new Action<GridLayoutGroup, string>((component, value) => component.spacing = new Vector2(component.spacing.x, Parse.Float(value))) },
         };
