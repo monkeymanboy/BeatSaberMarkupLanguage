7a1aa39b27d34a141a21eb5c050d11c0983e6560
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-04 16:23:04 +0200

    Implement title gradient background, and add setting gradient colors on the backgroundable imageview

--- BeatSaberMarkupLanguage/TypeHandlers/BackgroundableHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/BackgroundableHandler.cs
@@ -12,23 +12,20 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
         {
             { "background", new[] { "bg", "background" } },
             { "backgroundColor", new[] { "bg-color", "background-color" } },
+            { "backgroundColor0", new[] {"bg-color0", "background-color0"}},
+            { "backgroundColor1", new[] {"bg-color1", "background-color1"}},
+            { "backgroundAlpha", new[] {"bg-alpha", "background-alpha"}}
         };
 
         public override Dictionary<string, Action<Backgroundable, string>> Setters => new()
         {
             { "background", new Action<Backgroundable, string>((component, value) => component.ApplyBackground(value)) },
-            { "backgroundColor", new Action<Backgroundable, string>(TrySetBackgroundColor) },
+            { "backgroundColor", new Action<Backgroundable, string>((component, value) => component.ApplyColor(ParseColor(value))) },
+            { "backgroundColor0", new Action<Backgroundable, string>((component, value) => component.ApplyColor0(ParseColor(value))) },
+            { "backgroundColor1", new Action<Backgroundable, string>((component, value) => component.ApplyColor1(ParseColor(value))) },
+            { "backgroundAlpha", new Action<Backgroundable, string>((component, value) => component.ApplyAlpha(float.Parse(value))) },
         };
 
-        public static void TrySetBackgroundColor(Backgroundable background, string colorStr)
-        {
-            if (colorStr == "none")
-            {
-                return;
-            }
-
-            ColorUtility.TryParseHtmlString(colorStr, out Color color);
-            background.background.color = color;
-        }
+        public static Color ParseColor(string colorStr) => ColorUtility.TryParseHtmlString(colorStr, out Color color) ? color : Color.white;
     }
 }
