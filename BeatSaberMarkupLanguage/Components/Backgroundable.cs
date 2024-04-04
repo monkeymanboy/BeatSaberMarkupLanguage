2da441fd7781db1eab5a560cef3d09204149c44c
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2024-04-05 00:58:00 +0200

    Implement title gradient background, and add setting gradient colors on the backgroundable imageview (#146)

--- BeatSaberMarkupLanguage/Components/Backgroundable.cs
+++ BeatSaberMarkupLanguage/Components/Backgroundable.cs
@@ -7,7 +7,7 @@ namespace BeatSaberMarkupLanguage.Components
 {
     public class Backgroundable : MonoBehaviour
     {
-        public Image background;
+        public ImageView background;
 
         private static readonly Dictionary<string, ImageView> BackgroundCache = new();
 
@@ -17,6 +17,7 @@ namespace BeatSaberMarkupLanguage.Components
             { "panel-top", "RoundRect10" },
             { "panel-fade-gradient", "RoundRect10Thin" },
             { "panel-top-gradient", "RoundRect10" },
+            { "title-gradient", "RoundRect10" },
         };
 
         private static Dictionary<string, string> ObjectNames => new()
@@ -25,6 +26,7 @@ namespace BeatSaberMarkupLanguage.Components
             { "panel-top", "BG" },
             { "panel-fade-gradient", "Background" },
             { "panel-top-gradient", "BG" },
+            { "title-gradient", "BG" },
         };
 
         private static Dictionary<string, string> ObjectParentNames => new()
@@ -33,6 +35,7 @@ namespace BeatSaberMarkupLanguage.Components
             { "panel-top", "PracticeButton" },
             { "panel-fade-gradient", "LevelListTableCell" },
             { "panel-top-gradient", "ActionButton" },
+            { "title-gradient", "TitleViewController" },
         };
 
         public void ApplyBackground(string name)
@@ -67,6 +70,70 @@ namespace BeatSaberMarkupLanguage.Components
             {
                 Logger.Log.Error($"Error loading background: '{name}'");
             }
+        }
+
+        public void ApplyColor(Color color)
+        {
+            if (background == null)
+            {
+                throw new BSMLException("Can't set color on null background!");
+            }
+
+            Color color0 = new Color(1, 1, 1, background.color0.a);
+            Color color1 = new Color(1, 1, 1, background.color1.a);
+            color.a = background.color.a;
+
+            background.gradient = false;
+            background.color0 = color0;
+            background.color1 = color1;
+            background.color = color;
+        }
+
+        public void ApplyGradient(Color color0, Color color1)
+        {
+            if (background == null)
+            {
+                throw new BSMLException("Can't set gradient on null background!");
+            }
+
+            Color color = new Color(1, 1, 1, background.color.a);
+
+            background.gradient = true;
+            background.color = color;
+            background.color0 = color0;
+            background.color1 = color1;
+        }
+
+        public void ApplyColor0(Color color0)
+        {
+            if (background == null)
+            {
+                throw new BSMLException("Can't set gradient on null background!");
+            }
+
+            ApplyGradient(color0, background.color1);
+        }
+
+        public void ApplyColor1(Color color1)
+        {
+            if (background == null)
+            {
+                throw new BSMLException("Can't set gradient on null background!");
+            }
+
+            ApplyGradient(background.color0, color1);
+        }
+
+        public void ApplyAlpha(float alpha)
+        {
+            if (background == null)
+            {
+                throw new BSMLException("Can't set gradient on null background!");
+            }
+
+            Color color = background.color;
+            color.a = alpha;
+            background.color = color;
         }
 
         private static ImageView FindTemplate(string name, string backgroundName)
