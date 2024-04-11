84b7dd68a5deb8f620052ca575347659a97091db
Nicolas Gnyra <nicolas@gnyra.com>
2024-04-11 17:37:25 -0400

    Improve padding handler

--- BeatSaberMarkupLanguage/Parse.cs
+++ BeatSaberMarkupLanguage/Parse.cs
@@ -92,7 +92,7 @@ namespace BeatSaberMarkupLanguage
         /// <param name="defaultZ">Z value used if the string only has two components.</param>
         /// <returns>A <see cref="UnityEngine.Vector3"/> representation of the string.</returns>
         /// <exception cref="ParseException">Thrown if the string cannot be parsed.</exception>
-        public static Vector3 Vector3(string s, float defaultZ = 0)
+        public static Vector3 Vector3(string s, float? defaultZ = null)
         {
             string[] parts = s.Split([' '], StringSplitOptions.RemoveEmptyEntries);
             float x;
@@ -107,7 +107,7 @@ namespace BeatSaberMarkupLanguage
                 case 2:
                     x = Float(parts[0]);
                     y = Float(parts[1]);
-                    z = defaultZ;
+                    z = defaultZ ?? x;
                     break;
                 case 3:
                     x = Float(parts[0]);
@@ -144,6 +144,11 @@ namespace BeatSaberMarkupLanguage
                     x = z = Float(parts[0]);
                     y = w = Float(parts[1]);
                     break;
+                case 3:
+                    x = Float(parts[0]);
+                    y = w = Float(parts[1]);
+                    z = Float(parts[2]);
+                    break;
                 case 4:
                     x = Float(parts[0]);
                     y = Float(parts[1]);
@@ -155,6 +160,48 @@ namespace BeatSaberMarkupLanguage
             }
 
             return new Vector4(x, y, z, w);
+        }
+
+        /// <summary>
+        /// Parse a string as a <see cref="UnityEngine.RectOffset"/>.
+        /// </summary>
+        /// <remarks>The order follows <see href="https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.UI.RectMask2D.html#UnityEngine_UI_RectMask2D_padding">the apparent convention of Unity UI for Vector4 representation of padding</see> (X = Left, Y = Bottom, Z = Right, W = Top).</remarks>
+        /// <param name="s">String to parse.</param>
+        /// <returns>A <see cref="UnityEngine.RectOffset"/> representation of the string.</returns>
+        /// <exception cref="ParseException">Thrown if the string cannot be parsed.</exception>
+        public static RectOffset RectOffset(string s)
+        {
+            string[] parts = s.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+            int left;
+            int bottom;
+            int right;
+            int top;
+
+            switch (parts.Length)
+            {
+                case 1:
+                    left = bottom = right = top = Int(parts[0]);
+                    break;
+                case 2:
+                    left = right = Int(parts[0]);
+                    bottom = top = Int(parts[1]);
+                    break;
+                case 3:
+                    left = Int(parts[0]);
+                    bottom = top = Int(parts[1]);
+                    right = Int(parts[2]);
+                    break;
+                case 4:
+                    left = Int(parts[0]);
+                    bottom = Int(parts[1]);
+                    right = Int(parts[2]);
+                    top = Int(parts[3]);
+                    break;
+                default:
+                    throw new ParseException("Unexpected number of components");
+            }
+
+            return new RectOffset(top, bottom, left, right);
         }
 
         /// <summary>
--- BeatSaberMarkupLanguage/TypeHandlers/LayoutGroupHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/LayoutGroupHandler.cs
@@ -16,8 +16,8 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
             { "padBottom", new[] { "pad-bottom" } },
             { "padLeft", new[] { "pad-left" } },
             { "padRight", new[] { "pad-right" } },
-            { "pad", new[] { "pad" } },
-            { "childAlign", new[] { "child-align" } },
+            { "pad", new[] { "padding", "pad" } },
+            { "childAlign", new[] { "child-alignment", "child-align" } },
         };
 
         public override void HandleType(ComponentTypeWithData componentType, BSMLParserParams parserParams)
@@ -25,11 +25,17 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
             LayoutGroup layoutGroup = (LayoutGroup)componentType.component;
             if (componentType.data.TryGetValue("pad", out string pad))
             {
-                int padding = Parse.Int(pad);
-                layoutGroup.padding = new RectOffset(padding, padding, padding, padding);
+                layoutGroup.padding = Parse.RectOffset(pad);
+            }
+            else
+            {
+                layoutGroup.padding = new RectOffset(
+                    componentType.data.TryGetValue("padLeft", out string padLeft) ? Parse.Int(padLeft) : layoutGroup.padding.left,
+                    componentType.data.TryGetValue("padRight", out string padRight) ? Parse.Int(padRight) : layoutGroup.padding.right,
+                    componentType.data.TryGetValue("padTop", out string padTop) ? Parse.Int(padTop) : layoutGroup.padding.top,
+                    componentType.data.TryGetValue("padBottom", out string padBottom) ? Parse.Int(padBottom) : layoutGroup.padding.bottom);
             }
 
-            layoutGroup.padding = new RectOffset(componentType.data.TryGetValue("padLeft", out string padLeft) ? Parse.Int(padLeft) : layoutGroup.padding.left, componentType.data.TryGetValue("padRight", out string padRight) ? Parse.Int(padRight) : layoutGroup.padding.right, componentType.data.TryGetValue("padTop", out string padTop) ? Parse.Int(padTop) : layoutGroup.padding.top, componentType.data.TryGetValue("padBottom", out string padBottom) ? Parse.Int(padBottom) : layoutGroup.padding.bottom);
             if (componentType.data.TryGetValue("childAlign", out string childAlign))
             {
                 layoutGroup.childAlignment = (TextAnchor)Enum.Parse(typeof(TextAnchor), childAlign);
