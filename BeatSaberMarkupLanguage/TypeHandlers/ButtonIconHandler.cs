e4ffb666198b14c6591ce2ff5eec56b13a61f912
Nicolas Gnyra <nicolas@gnyra.com>
2024-04-12 00:29:49 -0400

    Add `sprite` to `icon-button`

--- BeatSaberMarkupLanguage/TypeHandlers/ButtonIconHandler.cs
+++ BeatSaberMarkupLanguage/TypeHandlers/ButtonIconHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Parser;
+using UnityEngine;
 
 namespace BeatSaberMarkupLanguage.TypeHandlers
 {
@@ -12,6 +14,7 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
             { "icon", new[] { "icon" } },
             { "iconSkew", new[] { "icon-skew" } },
             { "showUnderline", new[] { "show-underline" } },
+            { "sprite", new[] { "sprite" } },
         };
 
         public override Dictionary<string, Action<ButtonIconImage, string>> Setters => new()
@@ -20,5 +23,22 @@ namespace BeatSaberMarkupLanguage.TypeHandlers
             { "iconSkew", new Action<ButtonIconImage, string>((image, value) => image.SetSkew(Parse.Float(value))) },
             { "showUnderline", new Action<ButtonIconImage, string>((image, value) => image.SetUnderlineActive(Parse.Bool(value))) },
         };
+
+        public override void HandleType(BSMLParser.ComponentTypeWithData componentType, BSMLParserParams parserParams)
+        {
+            base.HandleType(componentType, parserParams);
+
+            ButtonIconImage buttonIconImage = (ButtonIconImage)componentType.component;
+
+            if (componentType.data.TryGetValue("sprite", out string sprite))
+            {
+                if (!parserParams.values.TryGetValue(sprite, out BSMLValue value))
+                {
+                    throw new ValueNotFoundException(sprite, parserParams.host);
+                }
+
+                buttonIconImage.image.sprite = value.GetValueAs<Sprite>();
+            }
+        }
     }
 }
